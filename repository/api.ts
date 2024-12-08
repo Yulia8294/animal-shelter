d49bd10d2b473fd2b47ec3ce2f69b0f44c6681a9
Yulia Novikova <ynovikova@buffergroup.com>
2024-12-09 02:03:39 +0300

    wip

--- repository/api.ts
+++ repository/api.ts
@@ -5,47 +5,58 @@ export async function fetchCatalog(
   numberOfItems?: number,
 ): Promise<PetDetails[]> {
   console.log("--------PATH", `${BASE_URL}/assets/data/database.json`);
-  const response: PetDetails[] = await fetch(
-    `${BASE_URL}/assets/data/database.json`,
-    { method: "get", cache: "no-cache" },
-  ).then((result) => result.json());
 
-  let result = response;
+  try {
+    const response: PetDetails[] = await fetch(
+      `${BASE_URL}/assets/data/database.json`,
+      { method: "get", cache: "no-cache" },
+    ).then((result) => result.json());
 
-  if (status) {
-    result = result.filter(
-      (item) => item.status && status.includes(item.status),
-    );
+    let result = response;
+
+    if (status) {
+      result = result.filter(
+        (item) => item.status && status.includes(item.status),
+      );
+    }
+
+    if (numberOfItems) {
+      result = result.slice(0, numberOfItems);
+    }
+
+    result.forEach((item) => {
+      item.traitsArray = item.traits?.split(",") ?? [];
+    });
+
+    result = await populatePhotos(result);
+
+    return result;
+  } catch (error: any) {
+    console.log("------- ERROR:", error);
+    throw new Error("Not found");
   }
-
-  if (numberOfItems) {
-    result = result.slice(0, numberOfItems);
-  }
-
-  result.forEach((item) => {
-    item.traitsArray = item.traits?.split(",") ?? [];
-  });
-
-  result = await populatePhotos(result);
-
-  return result;
 }
 
 export async function fetchItemBySlug(slug: string): Promise<PetDetails> {
-  const allPets: PetDetails[] = await fetch(
-    `${BASE_URL}/assets/data/database.json`,
-    { method: "get" },
-  ).then((result) => result.json());
+  try {
+    const allPets: PetDetails[] = await fetch(
+      `${BASE_URL}/assets/data/database.json`,
+      { method: "get" },
+    ).then((result) => result.json());
 
-  let pet = allPets.find((pet) => pet.slug === slug);
+    let pet = allPets.find((pet) => pet.slug === slug);
 
-  if (pet) {
-    pet = await populatePhotosForItem(pet);
-  } else {
+    if (pet) {
+      pet = await populatePhotosForItem(pet);
+    } else {
+      throw new Error("Not found");
+    }
+
+    return pet;
+  } catch (error: any) {
+    console.log("------- ERROR:", error);
     throw new Error("Not found");
   }
-
-  return pet;
 }
 
 async function populatePhotos(pets: PetDetails[]): Promise<PetDetails[]> {
@@ -56,17 +67,17 @@ async function populatePhotos(pets: PetDetails[]): Promise<PetDetails[]> {
 }
 
 async function populatePhotosForItem(pet: PetDetails): Promise<PetDetails> {
-  try {
-    console.log("Fetching for", pet.slug);
+  // try {
+  //   console.log("Fetching for", pet.slug);
 
-    const imageResponse = await fetch(`${BASE_URL}/api/media/${pet.slug}`);
-    const imageUrls: string[] = await imageResponse.json(); // Get the list of image URLs
+  //   const imageResponse = await fetch(`${BASE_URL}/api/media/${pet.slug}`);
+  //   const imageUrls: string[] = await imageResponse.json(); // Get the list of image URLs
 
-    pet.images = imageUrls; // Assign the URLs to the pet's `images` property
-    pet.mainImage = imageUrls[0];
-  } catch (error) {
-    pet.images = [];
-  }
+  //   pet.images = imageUrls; // Assign the URLs to the pet's `images` property
+  //   pet.mainImage = imageUrls[0];
+  // } catch (error) {
+  //   pet.images = [];
+  // }
 
   return pet;
 }
